89645574ad3115104e02410a877aa97dac54ff26
Dilek Beste Dülger <75923318+besteien@users.noreply.github.com>
2021-01-31 12:47:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names = new MyDictionary<string>();
+            names.Add("Mary");
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Jessica");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
